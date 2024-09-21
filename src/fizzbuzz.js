cf92bb3856f563fbb70d77d060d57596400c2b35
Victor <victor.chicinas@gmail.com>
2024-09-21 18:42:16 +0200

    done

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,19 @@
-const answer = []
+const answer = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 // Write your code below this line
 
+answer.forEach((element, index, array) => {
 
+    if(element % 3 === 0 && element % 5 === 0) {
+        array[index] = 'FizzBuzz'
+    }else if(element % 3 === 0) {
+        array[index] = 'Fizz'
+    }else if(element % 5 === 0) {
+        array[index]= 'Buzz'
+    }
+
+
+});
 
 
 
